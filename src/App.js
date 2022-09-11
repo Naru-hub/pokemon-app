75fdac990c252580489c8eea3ff6cc2af0c018b2
Naru-hub <1208ny0324@gmail.com>
2022-09-11 22:32:20 +0900

    前のページのポケモンを検索するボタンを作成

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
   const [nextURL, setNextURL] = useState("");
+  const [prevURL, setPrevURL] = useState("");
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -16,8 +17,9 @@ function App() {
       let res = await getAllPokemon(initialURL);
       // 各ポケモンの詳細なデータを取得
       loadPokemon(res.results);
-      console.log(res.next);
+      // console.log(res.next);
       setNextURL(res.next);
+      setPrevURL(res.Previous);
       setLoading(false);
     };
     fetchPokemonData();
@@ -42,10 +44,20 @@ function App() {
     // console.log(data);
     await loadPokemon(data.results);
     setNextURL(data.next);
+    setPrevURL(data.previous);
     setLoading(false);
   };
 
-  const handlePrevPage = () => {};
+  const handlePrevPage = async () => {
+    if (!prevURL) return;
+
+    setLoading(true);
+    let data = await getAllPokemon(prevURL);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
 
   return (
     <>
